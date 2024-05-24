cbb240766c7a62f74224b2a2c3d04850d4bf7304
K-DESKTOP\Krus <SelfishKrus@github.com>
2024-05-24 13:26:37 +0800

    <feature> live paramter

--- bpy/RopeAttacher.py
+++ bpy/RopeAttacher.py
@@ -12,16 +12,21 @@ class OBJECT_OT_rope_attacher(bpy.types.Operator):
 
     def execute(self, context):
         # pre
-        curve_obj = context.scene.curve_obj.copy()
         mesh_obj = context.scene.mesh_obj
-        smooth_factor = context.scene.smooth_factor
-        smooth_iterations = context.scene.smooth_iterations
+        # smooth_factor = context.scene.smooth_factor
+        # smooth_iterations = context.scene.smooth_iterations
         curve_extrude = context.scene.curve_extrude
         curve_depth = context.scene.curve_depth
         curve_cyclic = context.scene.curve_cyclic
 
-
-        # link
+        # copy curve_obj
+        curve_obj_name = context.scene.curve_obj.name + "_copy"
+        # delete
+        if curve_obj_name in bpy.data.objects:
+            bpy.data.objects.remove(bpy.data.objects[curve_obj_name], do_unlink=True)
+        curve_obj = context.scene.curve_obj.copy()
+        curve_obj.data = context.scene.curve_obj.data.copy()
+        curve_obj.name = curve_obj_name
         bpy.context.collection.objects.link(curve_obj)
 
         # select curve_obj
@@ -49,7 +54,8 @@ class OBJECT_OT_rope_attacher(bpy.types.Operator):
         bpy.context.object.data.extrude = curve_extrude
         bpy.context.object.data.bevel_depth = curve_depth
         bpy.context.object.data.splines[0].use_cyclic_u = curve_cyclic
-
+        bpy.context.object.data.splines[0].use_bezier_u = False
+        bpy.context.object.data.splines[0].use_endpoint_u = False
 
         # deselect all
         bpy.ops.object.select_all(action='DESELECT')
@@ -86,9 +92,19 @@ class OBJECT_PT_rope_attacher(bpy.types.Panel):
         box3.prop(context.scene, "curve_extrude")
         box3.prop(context.scene, "curve_cyclic")
         
-
-        # smooth 
         layout.operator("object.rope_attacher")
+
+def update_property(self, context):
+    curve_obj = context.scene.curve_obj
+    if curve_obj:
+        # Use getattr to get the value of the property with the same name as the update function
+        for mod in curve_obj.modifiers:
+            if hasattr(mod, self.bl_rna.identifier):
+                setattr(mod, self.bl_rna.identifier, getattr(context.scene, self.bl_rna.identifier))
+
+    # Trigger viewport update
+    bpy.ops.object.rope_attacher()
+    context.view_layer.update()
 
 def register():
     bpy.utils.register_class(OBJECT_OT_rope_attacher)
@@ -97,13 +113,13 @@ def register():
     bpy.types.Scene.curve_obj = bpy.props.PointerProperty(type=bpy.types.Object, name="Curve", description="Curve to snap to the mesh", poll=lambda self, obj: obj.type == 'CURVE')
     bpy.types.Scene.mesh_obj = bpy.props.PointerProperty(type=bpy.types.Object, name="Mesh", description="Mesh to snap the curve to", poll=lambda self, obj: obj.type == 'MESH')
     
-    bpy.types.Scene.offset = bpy.props.FloatProperty(name="Offset", description="Offset distance", default=0.0)
+    bpy.types.Scene.offset = bpy.props.FloatProperty(name="Offset", description="Offset distance", default=0.0, update=update_property)
     # bpy.types.Scene.smooth_factor = bpy.props.FloatProperty(name="Smooth Factor", description="Smooth factor", default=2.0)
     # bpy.types.Scene.smooth_iterations = bpy.props.IntProperty(name="Smooth Iterations", description="Smooth iterations", default=20)
 
-    bpy.types.Scene.curve_depth = bpy.props.FloatProperty(name="Curve Depth", description="Curve depth", default=0.05)
-    bpy.types.Scene.curve_extrude = bpy.props.FloatProperty(name="Curve Extrude", description="Curve extrude", default=0.0)
-    bpy.types.Scene.curve_cyclic = bpy.props.BoolProperty(name="Curve Cyclic", description="Curve cyclic", default=False)
+    bpy.types.Scene.curve_depth = bpy.props.FloatProperty(name="Curve Depth", description="Curve depth", default=0.05, update=update_property)
+    bpy.types.Scene.curve_extrude = bpy.props.FloatProperty(name="Curve Extrude", description="Curve extrude", default=0.0, update=update_property)
+    bpy.types.Scene.curve_cyclic = bpy.props.BoolProperty(name="Curve Cyclic", description="Curve cyclic", default=False, update=update_property)
 
 def unregister():
     bpy.utils.unregister_class(OBJECT_OT_rope_attacher)
